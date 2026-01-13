572885d5a1a8943e22cf560f4877c829bb5cdcae
Code-Egg <eric780217@gmail.com>
2026-01-13 14:29:41 +0800

    test

--- src/django_lscache/decorators.py
+++ src/django_lscache/decorators.py
@@ -1,13 +1,11 @@
 from functools import wraps
 from django.conf import settings
 
-def lscache(max_age=None, cacheability=None, esi=None):
+def lscache(max_age=None, cacheability=None, esi=False):
     if max_age is None:
         max_age = getattr(settings, "LSCACHE_DEFAULT_MAX_AGE", 60)
     if cacheability is None:
         cacheability = getattr(settings, "LSCACHE_DEFAULT_CACHEABILITY", "public")
-    if esi is None:
-        esi = getattr(settings, "LSCACHE_DEFAULT_ESI", False)
 
     def decorator(view_func):
         @wraps(view_func)
@@ -17,11 +15,13 @@ def lscache(max_age=None, cacheability=None, esi=None):
             if any(cookie in request.COOKIES for cookie in getattr(settings, "LSCACHE_SKIP_COOKIES", [])):
                 response["X-LiteSpeed-Cache-Control"] = "no-cache"
             else:
-                control = f"max-age={max_age},{cacheability}"
+                header_value = f"max-age={max_age},{cacheability}"
                 if esi:
-                    control += ",esi=on"
-                response["X-LiteSpeed-Cache-Control"] = control
+                    header_value += ",esi=on"
+                response["X-LiteSpeed-Cache-Control"] = header_value
 
             return response
+
         return wrapper
+
     return decorator
